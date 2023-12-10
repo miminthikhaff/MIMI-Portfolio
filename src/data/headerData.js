918faaa3c75f0d868d34931ebd0dab13ce8f7a0a
miminthikhaff github <you@example.com>
2023-12-10 19:32:07 +0530

    changes in headerData

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Mohamed-Inthikhaff',
-    title: "Web Developer",
+    title: "Software Engineer Undergraduate",
     desciption:"I am an expert in building consumer-focused websites and web applications that meet the highest standards for design, user experience, and performance. My skills include proficiency in HTML, CSS, JavaScript, React, Node.js, and MongoDB. I am a solutions-driven problem solver with a proven track record of delivering creative solutions that exceed client expectations. With strong project management skills, I am committed to staying up-to-date with the latest web development trends and best practices. I am excited to bring my skills and experience to your team and contribute to your organization's success. ",
     image: 'https://raw.githubusercontent.com/miminthikhaff/myphoto/main/porfolioDP.jpg',
 
